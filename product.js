9dddfdafbc74f7cb82a57e830140567437cf89a6
Kazemiee <92108455+Kazemiee@users.noreply.github.com>
2021-10-11 19:48:28 -0400

    Made a page Grimdark

--- product.js
+++ product.js
@@ -6,3 +6,18 @@ function ShowExtraDetails() {
     x.style.display = "none";
   }
 }
+
+function TogglePsychosis() {
+  var sch1 = document.getElementById("schizophrenia");
+  var sch2 = document.getElementById("not-schizophrenia");
+  var style = document.getElementById("style");
+  if (sch1.style.display === "none") {
+    sch1.style.display = "block";
+    sch2.style.display = "none";
+    style.href = "grimdark-style.css";
+  } else {
+    sch2.style.display = "block";
+    sch1.style.display = "none";
+    style.href = "style.css";
+  }
+}
