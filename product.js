f1bd9cfb250bda55cff23cbaadf3fb6424f05d1e
AlexZabski <54508987+AlexZabski@users.noreply.github.com>
2021-11-07 18:51:55 -0500

    Update product.js

--- product.js
+++ product.js
@@ -6,7 +6,7 @@ function ShowExtraDetails() {
     x.style.display = "none";
   }
 }
-
+ 
 function TogglePsychosis() {
   var sch1 = document.getElementById("schizophrenia");
   var sch2 = document.getElementById("not-schizophrenia");
