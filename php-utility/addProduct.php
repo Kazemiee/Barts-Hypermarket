647985937b74a46de23a641ab66d03b0ecdd7a1d
Matthew Kazemie <matkaz10@gmail.com>
2021-12-03 23:33:12 -0500

    Adding Products is Being Added

--- php-utility/addProduct.php
+++ php-utility/addProduct.php
@@ -1,10 +1,32 @@
 <?php
-$id;
-$aisle;
-$name;
-$important_details;
-$extra_details;
-$typeID;
-$tname;
-$multiplier;
+
+$file = "../product-xml/products.xml";
+// $id = (int)$_POST["id"];
+$aisle = $_POST["new-aisle"];
+$name = $_POST["new-name"];
+$image = $_POST["new-image"];
+$important_details = $_POST["i-details"];
+$extra_details = $_POST["e-details"];
+$typeID = $_POST["typeid"];
+$tname = $_POST["tname"];
+$multiplier = $_POST["p-multiplier"];
+$price = $_POST["price"];
+$adjusted_price = $_POST["a-price"];
+
+
+
+$xml = simplexml_load_file($file);
+$product = $xml->addChild("product");
+$product->addChild("name", $name);
+$product->addChild("image", $image);
+$product->addChild("aisle", $aisle);
+$product->addChild("important_details");
+$product->$important_details->addCData($important_details);
+$product->addChild("extra_details");
+$product->$extra_details->addCData($extra_details);
+$product->addChild("price", $price);
+$product->addChild("adjusted_price", $adjusted_price);
+$types = $product->addChild("types");
+$types->addChild("typeID", $typeID);
+$types->addChild("tname", $tname);
 ?>
