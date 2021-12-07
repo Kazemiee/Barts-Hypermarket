cf296eb506589affeab3fd668b8a3c85034fd6fa
Matthew Kazemie <matkaz10@gmail.com>
2021-12-07 11:58:34 -0500

    Update footer.php

--- php-templates/footer.php
+++ php-templates/footer.php
@@ -6,15 +6,15 @@
     <a href="#">Shareholder Relations </a>
     <a href="#">Corporate Website </a>
   </nav>
-    <a href="#">Spooky Backend Links Below</a>
+    <!-- <a href="#">Spooky Backend Links Below</a>
     <a href="#">No Clicking Allowed </a>
   <nav>
     <a href="product-list.php">Product List </a>
-    <a href="user-list.html">User List</a>
+    <a href="user-list.php">User List</a>
     <a href="edit-product.php">Edit a Product </a>
     <a href="order-list.html">Order List </a>
     <a href="edit-user-profile.html">Edit a Profile </a>
     <a href="edit-order-profile.html">Edit an Order </a>
-  </nav>
+  </nav> -->
     <p class="copyright">Copyright &copy; Today by Me</p>
 </footer>
