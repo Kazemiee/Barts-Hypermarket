23c811129d7616838a0c7508ebfd093d51abe739
Matthew Kazemie <matkaz10@gmail.com>
2021-12-07 11:56:46 -0500

    Update back-header.php

--- php-templates/back-header.php
+++ php-templates/back-header.php
@@ -9,7 +9,7 @@
       <h4>Username</h4>
       <nav class="top-nav">
         <a href="product-list.php">Product List</a>
-        <a href="user-list.html">User List</a>
+        <a href="user-list.php">User List</a>
         <a href="order-list.html">Orders</a>
         <a href="index.php">Store Front Page</a>
       </nav>
